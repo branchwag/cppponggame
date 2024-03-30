804a1a100647c792dee509801c46ce0f25c5bae4
LeAnne B <branchleannelmb@gmail.com>
2024-03-30 18:15:57 -0400

    refactors ball to be class

--- main.cpp
+++ main.cpp
@@ -3,39 +3,60 @@
 
 using namespace std;
 
+
+class Ball{
+    public:
+        float x, y;
+        int speed_x, speed_y;
+        int radius;
+    
+    void Draw(){
+        DrawCircle(x, y, radius, WHITE);
+    }
+
+    void Update(){
+        x += speed_x;
+        y += speed_y;
+
+        if(x + radius >= GetScreenWidth() || x - radius <= 0)
+        {
+            speed_x *= -1;
+        }
+
+        if(y + radius >= GetScreenHeight()  || y - radius <= 0)
+        {
+            speed_y *= -1;
+        }
+    }
+
+};
+
+Ball ball;
+
 int main () {
 
     const int screenWidth = 1280;
     const int screenHeight = 800;
-    int ball_x = 100;
-    int ball_y = 100;
-    int ball_speed_x = 5;
-    int ball_speed_y = 5;
-    int ball_radius = 15;
 
     InitWindow(screenWidth, screenHeight, "Pong");
     SetTargetFPS(60);
+    
+    ball.radius = 20;
+    ball.x = screenWidth/2;
+    ball.y = screenHeight/2;
+    ball.speed_x = 7;
+    ball.speed_y = 7;
 
     while (WindowShouldClose() == false){
         BeginDrawing();
         ClearBackground(BLACK);
-        ball_x += ball_speed_x;
-        ball_y += ball_speed_y;
 
-        if(ball_x + ball_radius >= screenWidth  || ball_x - ball_radius <= 0)
-        {
-            ball_speed_x *= -1;
-        }
+        ball.Update();
 
-        if(ball_y + ball_radius >= screenHeight  || ball_y - ball_radius <= 0)
-        {
-            ball_speed_y *= -1;
-        }
-
-        DrawCircle(ball_x,ball_y,ball_radius, WHITE);
+        DrawLine(screenWidth/2, 0, screenWidth/2, screenHeight, WHITE);
+        ball.Draw();
         DrawRectangle(10, screenHeight/2 - 120/2, 25, 120, WHITE);
         DrawRectangle(screenWidth - 35, screenHeight /2 - 60, 25, 120, WHITE);
-        DrawLine(screenWidth/2, 0, screenWidth/2, screenHeight, WHITE);
         EndDrawing();
     }
 
